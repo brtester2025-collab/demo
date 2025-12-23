58d65c608261b4d6ba18785113fee11d1aec9ecf
brtester2025-collab <brtester2025@gmail.com>
2025-12-23 12:32:43 +0530

    Day 1 file updated

---
+++ Syntax/Error.test.js
@@ -0,0 +1,17 @@
+function divide(a, b) {
+    if (b == 1) {
+        throw new Error("invalid")
+    }
+    return a / b;
+
+}
+
+test('throw new error', () => {
+    expect(() => divide(10, 1)).toThrow('invalid');
+})
+
+// test('check the return value', () => {
+//     expect(() => divide(10, 1)).toBe(a / b)
+// })
+
+
