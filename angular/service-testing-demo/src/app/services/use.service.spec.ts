f16fc1cd238ebda4f8d893b75bcf4f0b7ea3d364
brtester2025-collab <brtester2025@gmail.com>
2026-01-03 18:20:41 +0530

    update

--- angular/service-testing-demo/src/app/services/use.service.spec.ts
+++ angular/service-testing-demo/src/app/services/use.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService, User } from './use.service';   // ← Fixed the import path + export both
+import { error } from 'node:console';
 
 describe('UserService', () => {
   let service: UserService;
@@ -35,9 +36,9 @@ describe('UserService', () => {
 
     // Act: call the service
     service.getAllUsers().subscribe(users => {
-      // expect(users).toHaveLength(2);
+      expect(users.length).toBe(2);
       expect(users).toEqual(dummyUsers);
-      // or: expect(users).toStrictEqual(dummyUsers);
+
     });
 
     // Assert: exactly one GET request to this URL
@@ -63,4 +64,43 @@ describe('UserService', () => {
       .expectOne(req => req.method === 'GET' && req.url === 'https://api.example.com/users')
       .flush(mockUsers);
   });
+
+
+
+
+  test('Api failure', () => {
+    const message = "error message"
+    service.getAllUsers().subscribe({
+      next: () => fail("should have failed with 500 error"),
+      error: (error) => {
+        expect(error.status).toBe(500)
+        expect(error.statusText).toBe(message)
+      }
+    })
+    const req = httpMock.expectOne("here api calling is taking place")
+    req.flush(message, { status: 500, statusText: message })
+  })
+
+  //   createUser(user: User): Observable < User >
+  //{
+  //     return this.http.post<User>(this.apiUrl, user);
+  // }
+
+  test('to check the user', () => {
+    const user: User = { id: 1, name: 'bob' }
+    service.createUser(user).subscribe(user1 => {
+      expect(user1).toEqual(user)
+    })
+    const req = httpMock.expectOne('URL')
+    expect(req.request.method).toBe('post')
+    expect(req.request.body).toEqual(user)
+    req.flush(newUser);
+
+  })
+
+
+
+
+
+
 });
--- angular/service-testing-demo/src/setup-jest.ts
+++ angular/service-testing-demo/src/setup-jest.ts
@@ -1,2 +1,6 @@
 // src/setup-jest.ts
 import 'jest-preset-angular/setup-jest';
+
+createUser(user: User): Observable < User > {
+    return this.http.post<User>(this.apiUrl, user);
+}
