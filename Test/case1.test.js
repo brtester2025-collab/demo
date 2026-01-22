e865d48895c4be1ffad3d515a6bd388160995b44
brtester2025-collab <brtester2025@gmail.com>
2026-01-22 18:01:51 +0530

    updated for api

--- Test/case1.test.js
+++ Test/case1.test.js
@@ -1,1 +1,85 @@
 import { test, expect } from '@playwright/test';
+
+
+
+test('Starting......', async ({ request }) => {
+    const response = await request.post('https://dummyjson.com/users/add', {
+        data: {
+            firstName: "Beckom",
+            lastName: "Tester",
+            maidenName: 'brUstinx',
+            age: 20,
+            gender: "male",
+            email: "beckom@mailinator.com",
+            phone: 668885858,
+            username: "beck",
+            password: 123456,
+            birthDate: "2024-01-04"
+        }
+    })
+    const body = await response.json()
+    console.log(body);
+
+    expect(response.status()).toBe(201)
+    expect(body).toMatchObject({
+        id: expect.any(Number),
+        firstName: expect.any(String),
+        eyeColor: expect.any(String),
+        email: expect.any(String),
+        birthDate: expect.any(String),
+        // image: expect.any(''),
+        // bloodGroup: expect.any(String),
+        // hair: expect.any({
+        //     color: expect.any(String),
+        //     type: expect.any(String)
+        // }),
+        // ip: expect.any(String),
+        // address: expect.any({
+        //     address: expect.any(String),
+        //     city: expect.any(String),
+        //     state: expect.any(String),
+        //     stateCode: expect(Number),
+        //     postalCode: expect.any(String),
+        //     coordinates: expect.any({
+        //         lat: expect.any(Number),
+        //         lng: expect.any(Number)
+        //     }),
+        //     country: expect.any(String)
+        // }),
+    })
+    // expect(body.address).toBeTruthy()
+    // expect(typeof body.eyeColor).toBe('string')
+    // expect(typeof body.address).toBe('string')
+    // expect(typeof body.address.state).toBe('string')
+    // expect(typeof body.address.address.postalCode).toBe('string')
+    console.log("The END");
+
+})
+
+
+
+
+
+
+
+// API TEMPLATE 
+
+/*
+import { test, expect } from '@playwright/test';
+
+test('API test name', async ({ request }) => {
+
+  // 1. Call API
+  const response = await request.post(URL, { data });
+
+  // 2. Status assertion
+  expect(response.status()).toBe(EXPECTED_STATUS);
+
+  // 3. Read body
+  const body = await response.json();
+
+  // 4. Body assertions
+  expect(body.someField).toBeDefined();
+});
+
+ */
---
+++ Test/case2.test.js
@@ -0,0 +1,82 @@
+import { test, expect } from '@playwright/test'
+test('Create user', async ({ request }) => {
+    const response = await request.post('https://dummyjson.com/users/add', {
+        data: {
+            "id": 101,
+            "firstName": "Beckom",
+            "hair": {
+                "color": "Brown",
+                "type": "Curly"
+            },
+            "address": {
+                "city": "Delhi"
+            }
+        }
+    })
+
+    expect(response.status()).toBe(201)
+})
+
+
+// Validate Nested Objected 
+
+test('validate nested hair object for first user', async ({ request }) => {
+    const response = await request.get('https://dummyjson.com/users')
+    expect(response.status()).toBe(200)
+
+    const body = await response.json()
+
+    // Validate users array
+    expect(Array.isArray(body.users)).toBe(true)
+    expect(body.users.length).toBeGreaterThan(0)
+
+    // Access first user
+    const user = body.users[0]
+
+    // Validate hair object
+    expect(user.hair).toBeDefined()
+    expect(typeof user.hair).toBe('object')
+    expect(typeof user.hair.color).toBe('string')
+    expect(typeof user.hair.type).toBe('string')
+})
+
+
+test('LOOP-LOOP', async ({ request }) => {
+    const response = await request.get('https://dummyjson.com/users')
+    expect(response.status()).toBe(200)
+    const body = await response.json();
+
+    for (const user of body.users) {
+        expect(user.hair).toBeTruthy();
+        expect(typeof user.hair.color).toBe('string');
+        expect(typeof user.hair.type).toBe('string')
+    }
+
+
+
+})
+
+
+test('LOOP-V2', async ({ request }) => {
+    const response = await request.get(`https://dummyjson.com/users`)
+    expect(response.status()).toBe(200)
+    const body = await response.json();
+
+    for (const user of body.users) {
+        expect(typeof user.address).toBeTruthy()
+        expect(typeof user.address.state).toBe('string')
+        expect(typeof user.address.address).toBe('string')
+        expect(typeof user.address.stateCode).toBe('string')
+        expect(typeof user.address.postalCode).toBe('string')
+        expect(typeof user.address.coordinates).toBeTruthy()
+        expect(typeof user.address.coordinates.lat).toBe('number')
+        expect(typeof user.address.coordinates.lng).toBe('number')
+
+    }
+
+})
+
+
+
+
+console.log("-----------------------------THE-START---------------------------------");
